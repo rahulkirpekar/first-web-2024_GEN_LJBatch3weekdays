31d6162c6e036162d0da4607048f066626394b5c
rahul kirpekar <rahul.kirpekar@gmail.com>
2025-12-01 17:37:28 +0530

    request type-get/port

--- src/main/java/com/royal/controller/LoginServlet.java
+++ src/main/java/com/royal/controller/LoginServlet.java
@@ -11,11 +11,13 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class LoginServlet extends HttpServlet
 {
+	
+	// htmlform(get)------servlet---doGet()---Works
 	@Override
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
 		PrintWriter out = 	response.getWriter();
-		out.print("<b>LoginServlet </b><br>");
+		out.print("<b>LoginServlet ---Hi I am Get Request--Handled By doGet()</b><br>");
 		
 		Enumeration<String> e = request.getParameterNames();// userName,password
 		
@@ -28,11 +30,40 @@ public class LoginServlet extends HttpServlet
 			out.print("<b>"+paramName+" </b>" + paramValue+"<br>");
 		}
 		
-		
-		
 //		String userName = request.getParameter("userName");
 //		String password = request.getParameter("password");
 		
 //		out.print("<b>password </b>" + password+"<br>");
+		
 	}
+	
+	// htmlform(post)-----servlet---doGet()---Works
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		doGet(request, response);
+	}
+	
+//	@Override
+//	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+//	{
+//		PrintWriter out = 	response.getWriter();
+//		out.print("<b>LoginServlet </b><br>");
+//		
+//		Enumeration<String> e = request.getParameterNames();// userName,password
+//		
+//		while(e.hasMoreElements()) 
+//		{
+//			String paramName = e.nextElement();
+//			
+//			String paramValue = request.getParameter(paramName);
+//			
+//			out.print("<b>"+paramName+" </b>" + paramValue+"<br>");
+//		}
+//		
+////		String userName = request.getParameter("userName");
+////		String password = request.getParameter("password");
+//		
+////		out.print("<b>password </b>" + password+"<br>");
+//	}
 }
