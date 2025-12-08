695658efba859f262a62ed565748a41c895f726c
rahul kirpekar <rahul.kirpekar@gmail.com>
2025-12-08 18:01:16 +0530

    login

--- src/main/java/com/royal/controller/FirstServlet.java
+++ src/main/java/com/royal/controller/FirstServlet.java
@@ -10,8 +10,30 @@ import jakarta.servlet.http.HttpServletResponse;
 public class FirstServlet extends HttpServlet
 {
 	@Override
+	public void init() throws ServletException 
+	{
+		System.out.println("FirstServlet---init()--- FirstServlet Object created : " + this);
+	}
+	
+	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
-		System.out.println("FirstServlet---service()---Hello World");
+		System.out.println("=============================================================");
+		System.out.println("FirstServlet---service()--- FirstServlet Object : " + this);
+		System.out.println("request : " + request);
+		
+		System.out.println("request.getRemoteHost() : " + request.getRemoteHost());
+		System.out.println("request.getRemoteAddr() : " + request.getRemoteAddr());
+		System.out.println("request.getRemotePort() : " + request.getRemotePort());
+		System.out.println("request.getRemoteUser() : " + request.getRemoteUser());
+		
+		System.out.println("response : " + response);
+		System.out.println("=============================================================");
+	}
+	
+	@Override
+	public void destroy() 
+	{
+		System.out.println("FirstServlet---destroy()---FirstServlet Object : " + this);
 	}
 }
--- src/main/java/com/royal/controller/SecondServlet.java
+++ src/main/java/com/royal/controller/SecondServlet.java
@@ -10,8 +10,31 @@ import jakarta.servlet.http.HttpServletResponse;
 public class SecondServlet extends HttpServlet
 {
 	@Override
+	public void init() throws ServletException 
+	{
+		System.out.println("SecondServlet---init()--- SecondServlet Object created : " + this);
+	}
+	
+	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
-		System.out.println("SecondServlet----service()---Hello World");
+		System.out.println("=============================================================");
+		System.out.println("SecondServlet---service()--- SecondServlet Object : " + this);
+		System.out.println("request : " + request);
+		
+		System.out.println("request.getRemoteHost() : " + request.getRemoteHost());
+		System.out.println("request.getRemoteAddr() : " + request.getRemoteAddr());
+		System.out.println("request.getRemotePort() : " + request.getRemotePort());
+		System.out.println("request.getRemoteUser() : " + request.getRemoteUser());
+		
+		
+		System.out.println("response : " + response);
+		System.out.println("=============================================================");
+	}
+	
+	@Override
+	public void destroy() 
+	{
+		System.out.println("SecondServlet---destroy()---SecondServlet Object : " + this);
 	}
 }
