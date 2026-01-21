2e9ae95e27ac29fbfd20f0c2b1e99301effc9e73
rahul kirpekar <rahul.kirpekar@gmail.com>
2026-01-21 17:59:54 +0530

    ListStudentServlet

--- src/main/java/com/royal/controller/ListStudentServlet.java
+++ src/main/java/com/royal/controller/ListStudentServlet.java
@@ -1,6 +1,10 @@
 package com.royal.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import com.royal.bean.StudentBean;
+import com.royal.dao.StudentDao;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -14,6 +18,14 @@ public class ListStudentServlet extends HttpServlet
 	{
 		System.out.println("ListStudentServlet : service()");
 		
+		StudentDao dao = new StudentDao();
+		
+		ArrayList<StudentBean> list = dao.getAllStudentRecords();
+		
+		request.setAttribute("list", list);
+		
+		System.out.println("ListStudentServlet records count: " + list.size());
+		
+		request.getRequestDispatcher("studlist.jsp").forward(request, response);
 	}
-
 }
--- src/main/java/com/royal/dao/StudentDao.java
+++ src/main/java/com/royal/dao/StudentDao.java
@@ -1,8 +1,10 @@
 package com.royal.dao;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import com.royal.bean.StudentBean;
 import com.royal.util.DBConnection;
@@ -52,4 +54,71 @@ public class StudentDao {
 		}
 		return rowsAffected ;	
 	}
+
+	public ArrayList<StudentBean> getAllStudentRecords() 
+	{
+		String selectQuery = "SELECT * FROM student";
+		
+		Connection conn = DBConnection.getDBInstance();
+		Statement stmt = null;
+		ResultSet rs = null;
+		StudentBean sbean = null;
+		ArrayList<StudentBean> list = new ArrayList<StudentBean>();
+		if (conn != null) 
+		{
+			try 
+			{
+				stmt = conn.createStatement();
+			
+				rs = stmt.executeQuery(selectQuery);
+				
+				while(rs.next()) 
+				{
+					int id = rs.getInt(1);
+					String fullName  = rs.getString(2);
+					int age = rs.getInt(3);
+					String course = rs.getString(4);
+					String gender = rs.getString(5);
+					String hobbiesStr = rs.getString(6);
+					String dob  = rs.getString(7);
+					String email = rs.getString(8);
+					String mobile = rs.getString(9);
+					String address = rs.getString(10);
+					
+					// String -- String Array --- ,
+					
+					String hobbies[] = hobbiesStr.split(",");
+					
+					sbean = new StudentBean(id, fullName, age, course, gender, hobbies, dob, email, mobile, address);
+					list.add(sbean);
+				}
+				
+			} catch (SQLException e) 
+			{
+				e.printStackTrace();
+			}
+		} else 
+		{
+			System.out.println("StudentDao---getAllStudentRecords() DB not connected");
+		}
+		return list;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
