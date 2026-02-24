af12241582ae057211e1589fe5b7b400ad0a42ab
Rahul Kirpekar <rahul.kirpekar@gmail.com>
2026-02-24 17:48:29 +0530

    servlet context listener

--- src/main/java/com/royal/controller/TwentyServlet.java
+++ src/main/java/com/royal/controller/TwentyServlet.java
@@ -2,6 +2,7 @@ package com.royal.controller;
 
 import java.io.IOException;
 
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,12 +14,12 @@ public class TwentyServlet extends HttpServlet
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
-		HttpSession session = request.getSession();
-		
-		session.setAttribute("userName", "Rahul Kirpekar");
-		
-		
+//		HttpSession session = request.getSession();
+//		session.setAttribute("userName", "Rahul Kirpekar");
 		System.out.println("TwentyServlet : service()");
 		
+		ServletContext context = getServletContext();
+		
+		context.setAttribute("test1", "Royal technosoft");
 	}
 }
---
+++ src/main/java/com/royal/listener/TestServletContextListener.java
@@ -0,0 +1,21 @@
+package com.royal.listener;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+
+public class TestServletContextListener implements ServletContextListener 
+{
+	@Override
+	public void contextInitialized(ServletContextEvent sce) 
+	{
+		System.out.println("TestServletContextListener : contextInitialized()");
+	}
+	
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) 
+	{
+		System.out.println("TestServletContextListener : contextDestroyed()");
+		
+	}
+
+}
